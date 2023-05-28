bfa8425e423df76b4dc30c20620daddd1487e94e
Ace <kronicrespawn@gmail.com>
2023-05-28 11:44:31 -0500

    fixed; backend connects sql and react

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
 const express = require('express');
+const cors = require("cors");
+const bodyParser = require('body-parser');
 const app = express();
 
 const con = mysql.createConnection({
@@ -19,11 +21,17 @@ con.connect(function(err){
     }
 })
 
-app.get("/", (req, res) => {
-    const sqlInsert = "INSERT INTO reviews (reviewTitle, review) VALUES ('GoGo Beats', 'It was okay but not really');"
-    con.query(sqlInsert, (err, result) => {
-        res.send("hello world");
-    });
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/api/insert", (req, res) => {
+    const reviewTitle = req.body.reviewTitle
+    const review = req.body.review
+    const sqlInsert = "INSERT INTO reviews (reviewTitle, review) VALUES (?,?)"
+    con.query(sqlInsert, [reviewTitle, review], (err, result) =>{
+        console.log(result)
+    })
 });
 
 app.listen(3001, () =>{
